99210734de9c6cf2b0d4bc6a1fc61fd0069fc6fd
Suman K <106149605+Sumank02@users.noreply.github.com>
2026-02-02 08:39:09 +0000

    container and element variables; removed an unused top-level totalAmount

--- cart.js
+++ cart.js
@@ -11,29 +11,29 @@ function updateBadge() {
 
 updateBadge()
 
-let cartContainer = document.getElementById('cartContainer')
+const cartContainer = document.getElementById('cartContainer')
 
-let boxContainerDiv = document.createElement('div')
+const boxContainerDiv = document.createElement('div')
 boxContainerDiv.id = 'boxContainer'
 
 // DYNAMIC CODE TO SHOW THE SELECTED ITEMS IN YOUR CART
 function dynamicCartSection(ob, itemCounter)
 {
-    let boxDiv = document.createElement('div')
+    const boxDiv = document.createElement('div')
     boxDiv.id = 'box'
     boxContainerDiv.appendChild(boxDiv)
 
-    let boxImg = document.createElement('img')
+    const boxImg = document.createElement('img')
     boxImg.src = ob.preview
     boxDiv.appendChild(boxImg)
 
-    let boxh3 = document.createElement('h3')
-    let h3Text = document.createTextNode(ob.name + ' × ' + itemCounter)
+    const boxh3 = document.createElement('h3')
+    const h3Text = document.createTextNode(ob.name + ' × ' + itemCounter)
     boxh3.appendChild(h3Text)
     boxDiv.appendChild(boxh3)
 
-    let boxh4 = document.createElement('h4')
-    let h4Text = document.createTextNode('Amount: Rs' + ob.price)
+    const boxh4 = document.createElement('h4')
+    const h4Text = document.createTextNode('Amount: Rs' + ob.price)
     boxh4.appendChild(h4Text)
     boxDiv.appendChild(boxh4)
 
@@ -43,36 +43,36 @@ function dynamicCartSection(ob, itemCounter)
     return cartContainer
 }
 
-let totalContainerDiv = document.createElement('div')
+const totalContainerDiv = document.createElement('div')
 totalContainerDiv.id = 'totalContainer'
 
-let totalDiv = document.createElement('div')
+const totalDiv = document.createElement('div')
 totalDiv.id = 'total'
 totalContainerDiv.appendChild(totalDiv)
 
-let totalh2 = document.createElement('h2')
-let h2Text = document.createTextNode('Total Amount')
+const totalh2 = document.createElement('h2')
+const h2Text = document.createTextNode('Total Amount')
 totalh2.appendChild(h2Text)
 totalDiv.appendChild(totalh2)
 
 // TO UPDATE THE TOTAL AMOUNT
 function amountUpdate(amount)
 {
-    let totalh4 = document.createElement('h4')
-    let totalh4Text = document.createTextNode('Amount: Rs ' + amount)
+    const totalh4 = document.createElement('h4')
+    const totalh4Text = document.createTextNode('Amount: Rs ' + amount)
     totalh4Text.id = 'toth4'
     totalh4.appendChild(totalh4Text)
     totalDiv.appendChild(totalh4)
 }
 
-let buttonDiv = document.createElement('div')
+const buttonDiv = document.createElement('div')
 buttonDiv.id = 'button'
 totalDiv.appendChild(buttonDiv)
 
-let buttonTag = document.createElement('button')
+const buttonTag = document.createElement('button')
 buttonDiv.appendChild(buttonTag)
 
-let buttonLink = document.createElement('a')
+const buttonLink = document.createElement('a')
 buttonLink.href = '/orderPlaced.html?'
 buttonTag.appendChild(buttonLink)
 
@@ -80,7 +80,6 @@ const buttonText = document.createTextNode('Place Order')
 buttonLink.appendChild(buttonText)
 
 // BACKEND CALL - Using Fetch API
-let totalAmount = 0
 fetch('https://5d76bf96515d1a0014085cf9.mockapi.io/product')
     .then(response => response.json())
     .then(contentTitle => {
