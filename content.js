3e2277a0ecd3a33ac885866390f96e72f0b228a3
Suman K <106149605+Sumank02@users.noreply.github.com>
2026-02-02 08:48:19 +0000

    Replaced let declarations with const (DOM element variables and containers)

--- content.js
+++ content.js
@@ -1,27 +1,27 @@
-let contentTitle;
+let contentTitle = [];
 function dynamicClothingSection(ob) {
-  let boxDiv = document.createElement("div");
+  const boxDiv = document.createElement("div");
   boxDiv.id = "box";
 
-  let boxLink = document.createElement("a");
+  const boxLink = document.createElement("a");
   boxLink.href = "/contentDetails.html?" + ob.id;
 
-  let imgTag = document.createElement("img");
+  const imgTag = document.createElement("img");
   imgTag.src = ob.preview;
 
-  let detailsDiv = document.createElement("div");
+  const detailsDiv = document.createElement("div");
   detailsDiv.id = "details";
 
-  let h3 = document.createElement("h3");
-  let h3Text = document.createTextNode(ob.name);
+  const h3 = document.createElement("h3");
+  const h3Text = document.createTextNode(ob.name);
   h3.appendChild(h3Text);
 
-  let h4 = document.createElement("h4");
-  let h4Text = document.createTextNode(ob.brand);
+  const h4 = document.createElement("h4");
+  const h4Text = document.createTextNode(ob.brand);
   h4.appendChild(h4Text);
 
-  let h2 = document.createElement("h2");
-  let h2Text = document.createTextNode("rs  " + ob.price);
+  const h2 = document.createElement("h2");
+  const h2Text = document.createTextNode("rs  " + ob.price);
   h2.appendChild(h2Text);
 
   boxDiv.appendChild(boxLink);
@@ -34,9 +34,9 @@ function dynamicClothingSection(ob) {
   return boxDiv;
 }
 
-let mainContainer = document.getElementById("mainContainer");
-let containerClothing = document.getElementById("containerClothing");
-let containerAccessories = document.getElementById("containerAccessories");
+const mainContainer = document.getElementById("mainContainer");
+const containerClothing = document.getElementById("containerClothing");
+const containerAccessories = document.getElementById("containerAccessories");
 
 // BACKEND CALLING - Using Fetch API
 fetch("https://5d76bf96515d1a0014085cf9.mockapi.io/product")
